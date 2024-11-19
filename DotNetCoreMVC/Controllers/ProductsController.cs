18a94666695d02f45bd3ad1588eaefe8936ee4bb
Taha Burak TÜZÜN <buraktuzun8046@gmail.com>
2024-11-19 11:56:36 +0300

    -SQL connection ayarlandı. -Db modelleri oluşturuldu.

---
+++ DotNetCoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using DotNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+
+            if(!_context.Products.Any())
+            {
+                _context.Products.Add(new Product { Name = "Product 1", Price = 100, Stock = 10 });
+                _context.Products.Add(new Product { Name = "Product 2", Price = 200, Stock = 20 });
+                _context.Products.Add(new Product { Name = "Product 3", Price = 300, Stock = 30 });
+                _context.Products.Add(new Product { Name = "Product 4", Price = 400, Stock = 40 });
+
+                _context.SaveChanges();
+            }
+
+            
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- DotNetCoreMVC/obj/Debug/net8.0/DotNetCoreMVC.AssemblyInfo.cs
+++ DotNetCoreMVC/obj/Debug/net8.0/DotNetCoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1ffc9b55c7a317cde7130e18132b6e2ac74d9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetCoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetCoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
